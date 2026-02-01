429b3016b6b73658f630f902cdf66b40756b8c99
Sailendra Reddy <sailendra.devops@gmail.com>
2026-02-01 22:55:27 +0530

    new changes

--- shell-scripting-notes/script-02.sh
+++ shell-scripting-notes/script-02.sh
@@ -2,7 +2,7 @@
 
 
 
-Suresh: Hi Ramesh how are you
-Ramesh: Good,how are you Suresh
-what are you practicing Ramesh
-I am parcticing shell-scripting Suresh
+echo "Suresh: Hi Ramesh how are you"
+echo "Ramesh: Good,how are you Suresh"
+echo "what are you practicing Ramesh"
+echo "I am parcticing shell-scripting Suresh"
