799782f57c773a5c45781abbf4d44ae744477fcb
Sailendra Reddy <sailendra.devops@gmail.com>
2026-02-01 23:02:06 +0530

    new changes

---
+++ shell-scripting-notes/script-03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=Wasi
+person2=Ravi
+
+echo "$person1: Hi $person2 how are you"
+echo "$person2: Good,how are you $person1"
+echo "$person1: what are you practicing $person2"
+echo "$person2: I am parcticing shell-scripting $person1"
