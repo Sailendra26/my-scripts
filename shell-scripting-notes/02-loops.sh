0d6afdafd27bc50ca1d6d80d90e3aee0a5f7bd72
Sailendra Reddy <sailendra.devops@gmail.com>
2026-02-22 17:43:03 +0530

    scripts updated

--- shell-scripting-notes/02-loops.sh
+++ shell-scripting-notes/02-loops.sh
@@ -13,13 +13,14 @@ LOG_FILE=$($LOG_FOLDER/$SCRIPT_NAME.log)
 
 mkdir -p $LOG_FOLDER
 
-if [ $USER_ID -ne 0]; then
+if [ $USER_ID -ne 0 ]; then
     echo " $R ERROR: please run wuth user privileges $N" 
     exit 1
 fi 
 
 VALIDATE() {
-    if [$1 -ne 0]; then
+
+    if [ $1 -ne 0 ]; then
         echo " $2 installation is ...$G FAILURE $N"
         exit 1
     else
@@ -31,7 +32,7 @@ VALIDATE() {
 for package in $@
 do
     dnf list installed $package &>> $LOG_FILE
-    if [ $? -ne 0]; then
+    if [ $? -ne 0 ]; then
         dnf list installed $package -y &>> $LOG_FILE
         VALIDATE $? "$package"
     else
