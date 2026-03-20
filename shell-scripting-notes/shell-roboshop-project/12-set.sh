35761aeb7bf53e820cf610ee8a0692de9f45b1ff
Sailendra Reddy <sailendra.devops@gmail.com>
2026-03-20 16:34:12 +0530

    message

---
+++ shell-scripting-notes/shell-roboshop-project/12-set.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# error(){
+#     echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
+# }
+
+echo "Hello.."
+echo "Before error.."
+ccaffjl;dnf # here shell understands there is an error and signal is ERR
+echo "After error"
