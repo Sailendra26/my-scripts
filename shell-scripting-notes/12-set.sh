5ca1276d26146bf1a8b24f064f57afba5cbf36ea
Sailendra Reddy <sailendra.devops@gmail.com>
2026-03-20 16:47:46 +0530

    message

--- shell-scripting-notes/12-set.sh
+++ shell-scripting-notes/12-set.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-# error(){
-#     echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
-# }
-
 echo "Hello.."
 echo "Before error.."
 ccaffjl;dnf # here shell understands there is an error and signal is ERR
