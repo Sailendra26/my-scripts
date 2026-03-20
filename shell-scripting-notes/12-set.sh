6d939cd1a47043a29a753d07e92b1f8b67cc5873
Sailendra Reddy <sailendra.devops@gmail.com>
2026-03-20 17:05:32 +0530

    message

--- shell-scripting-notes/12-set.sh
+++ shell-scripting-notes/12-set.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-echo "Hello.."
-echo "Before error.."
-ccaffjl;dnf 
-echo "After error"
+# echo "Hello.."
+# echo "Before error.."
+# ccaffjl;dnf 
+# echo "After error"
+
+echo "Starting..."
+echo "hello" | grep "abc" | wc -l
+echo "After pipe"
