a31b9b10aada485edec009b3843a835bbe4bad30
Sailendra Reddy <sailendra.devops@gmail.com>
2026-03-21 22:28:15 +0530

    updating services

--- shell-scripting-notes/shell-roboshop-project/rabbitmq.sh
+++ shell-scripting-notes/shell-roboshop-project/rabbitmq.sh
@@ -10,14 +10,16 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-mkdir -p $LOGS_FOLDER
-SCRIPT_DIR=$PWD
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
+
+mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
--- shell-scripting-notes/shell-roboshop-project/redis.sh
+++ shell-scripting-notes/shell-roboshop-project/redis.sh
@@ -10,6 +10,12 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
