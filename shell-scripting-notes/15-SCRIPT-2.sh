0c02e874ea117ce924e6117aa987b7f27dd36a42
Sailendra Reddy <sailendra.devops@gmail.com>
2026-05-06 13:23:28 +0530

    loop script

--- shell-scripting-notes/15-SCRIPT-2.sh
+++ shell-scripting-notes/15-SCRIPT-2.sh
@@ -3,4 +3,5 @@
 echo " I Love : $EVERYTHING "
   echo " Megham is teaching Devops "
 echo "I am learning Devops"
-  echo "PID of SCRIPT-2: $$"
+echo "PID of SCRIPT-2: $$"
+        /echo "move forward"
