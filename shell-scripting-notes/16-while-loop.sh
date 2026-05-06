fd99eedd08e16bc3354763b4150802576308601e
Sailendra Reddy <sailendra.devops@gmail.com>
2026-05-06 11:34:54 +0530

    loop script

--- shell-scripting-notes/16-while-loop.sh
+++ shell-scripting-notes/16-while-loop.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-i=1
-while [ $i -le 5 ]
+#Example-1: print 1 to 5
+
+# i=1
+# while [ $i -le 5 ]
+# do
+#   echo $i
+#   i=$((i+1)) # Increment the count
+# done
+
+
+#Example-2: checking count down 
+
+count=5
+echo "Starting countdown..."
+while [ $count -gt 0 ]
 do
-  echo $i
-  i=$((i+1))
+  echo "Time left: $count"
+  sleep 2 # Pause for 1 second
+  count=$((count - 1)) # Decrement the count
 done
